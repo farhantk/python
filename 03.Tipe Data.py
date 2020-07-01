0d8a418f8ce2bd7e43b559d89203245722112af7
farhantk <64940605+farhantk@users.noreply.github.com>
2020-07-01 14:08:46 +0700

    Macam-macam tipe data

---
+++ 03.Tipe Data.py
@@ -0,0 +1,24 @@
+# Integer, untuk bilangan bulat
+x = 5
+print(x)
+print("-Tipe data =", type(x))
+print("=" *100)
+#Float, untuk bilangan desimal
+y = 5.5
+print(y)
+print("-tipe data =", type(y))
+print("=" *100)
+
+#String, untuk kumpulan karekter/kata
+z = "mobil"
+print("z")
+print("-tipe data =", type(z))
+print("=" *100)
+
+#Boolean, untuk True/False
+p = True
+q = False
+print(p)
+print("-tipe data =", type(p))
+print(q)
+print("-tipe data =", type(q))
