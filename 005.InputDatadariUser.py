535e6034a336612e0dc545691b01c2d623446511
farhantk <64940605+farhantk@users.noreply.github.com>
2020-07-02 20:49:46 +0700

    Membuat input dari user

---
+++ 005.InputDatadariUser.py
@@ -0,0 +1,6 @@
+x = input("Masukan nama : ") # tipenya pasti str
+y = int(input("Masukan umur : ")) 
+z = float(input("Masukan tinggi badan : "))
+print("Nama : ", x, type(x))
+print("Umur : ", y, " Tahun", type(y))
+print("Umur : ", z, " M", type(z))
