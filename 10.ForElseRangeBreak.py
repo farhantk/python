2a2f7e4eb13f1baec81087b71fccfed9eeeadd3a
farhantk <64940605+farhantk@users.noreply.github.com>
2020-07-14 08:39:46 +0700

    For else, range, break

---
+++ 10.ForElseRangeBreak.py
@@ -0,0 +1,11 @@
+
+for i in range(0,20):
+    print(i)
+else:
+    print("selesai")
+
+for i in range(0,10):
+    print(i)
+    if i is 8:
+        print("ini angka 8")
+        break # Break, untuk berhenti saat angka ditemukan
