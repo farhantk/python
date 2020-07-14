b3a33431b295d75f883c28890a9dc9914e19743f
farhantk <64940605+farhantk@users.noreply.github.com>
2020-07-14 09:09:12 +0700

    While Loop

---
+++ 11.WhileLoop.py
@@ -0,0 +1,14 @@
+x = 5
+while x < 6:
+    print("loop terus")
+    x += 1
+
+angka = 0
+mulai = True
+while mulai:
+    print("while dimulai ",angka)
+    angka += 1
+    if angka is 10:
+        mulai = False
+        print("stop")
+        
